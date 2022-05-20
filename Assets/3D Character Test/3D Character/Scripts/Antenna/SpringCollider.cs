49ac230d0f9d79b879fcdb1fb770147196924092
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2022-05-21 01:36:27 +0530

    Antenna Physics

---
+++ Assets/3D Character Test/3D Character/Scripts/Antenna/SpringCollider.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+    public class SpringCollider : MonoBehaviour
+    {
+        //半径
+        public float radius = 0.5f;
+
+        private void OnDrawGizmosSelected ()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere (transform.position, radius);
+        }
+    }
+}
