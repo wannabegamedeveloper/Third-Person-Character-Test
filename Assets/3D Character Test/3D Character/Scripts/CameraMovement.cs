a43b9c0440427995089ae63d760f65fbfefbe2dd
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2022-05-21 00:47:29 +0530

    Bending on X Mouse Movement

--- Assets/3D Character Test/3D Character/Scripts/CameraMovement.cs
+++ Assets/3D Character Test/3D Character/Scripts/CameraMovement.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform point;
+    [SerializeField] private Transform player;
+
+    private Transform _transform;
+    
+    private void Start()
     {
-        
+        _transform = player;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        var position = _transform.position;
+        var pointPosition = transform;
+        Vector3 pos = new Vector3(position.x, pointPosition.position.y, position.z);
+
+        pointPosition.position = pos;
         
+        float mouseX = Input.GetAxis("Mouse Y");
+        float mouseY = Input.GetAxis("Mouse X");
+        
+        point.Rotate(new Vector3(-mouseX, 0f, 0f));
+        transform.Rotate(new Vector3(0f, mouseY, 0f));
     }
 }
--- Assets/3D Character Test/3D Character/Scripts/ThirdPersonController.cs
+++ Assets/3D Character Test/3D Character/Scripts/ThirdPersonController.cs
@@ -6,19 +6,21 @@ public class ThirdPersonController : MonoBehaviour
     [SerializeField] private float animationTransitionTime;
     [SerializeField] private Transform constraint;
     [SerializeField] private float bendAmount;
+    [SerializeField] private Transform followPoint;
     
     private Vector3 _movement;
     private CharacterController _controller;
     private Animator _characterAnimator;
     private float _lerpingSpeed;
     private static readonly int X = Animator.StringToHash("X");
-
-    private bool movedBack;
-
+    
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
         _characterAnimator = GetComponent<Animator>();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
@@ -42,19 +44,22 @@ public class ThirdPersonController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
 
-        if (moveY < 0f && !movedBack)
+        if (Mathf.Abs(moveX) > 0f || Mathf.Abs(moveY) > 0f)
         {
-            transform.Rotate(0f, 0f, 180f);
-            movedBack = true;
-        }
-        else if (moveY == 0f)
-            movedBack = false;
-        
-        if (Mathf.Abs(moveX) > 0f && Mathf.Abs(moveY) > 0f)
+            transform.rotation = Quaternion.Lerp(transform.rotation, followPoint.localRotation, 10f * Time.deltaTime);
+            
+            float mouseY = Input.GetAxis("Mouse X");
+            
+            if (mouseY > 3f) return;
+            
             constraint.localRotation =
-                Quaternion.Lerp(constraint.localRotation, Quaternion.Euler(0f, 0f, moveX * -bendAmount), 10f * Time.deltaTime);
+                Quaternion.Lerp(constraint.localRotation, Quaternion.Euler(0f, 0f, mouseY * -bendAmount),
+                    10f * Time.deltaTime);
+        }
+        else
+            constraint.localRotation = Quaternion.identity;
         
-        _movement = new Vector3(moveX, 0f,   Mathf.Abs(moveY));
+        _movement = new Vector3(moveX, 0f, moveY);
         Vector3 localMovement = transform.TransformDirection(_movement);
         
         _controller.Move(localMovement * (speed * Time.deltaTime));
