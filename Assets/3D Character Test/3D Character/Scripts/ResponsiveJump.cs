341de406ea1f5d0ae60292daf2101afebf09d928
TusharVaid30 <31384470+TusharVaid30@users.noreply.github.com>
2022-07-09 22:01:41 +0530

    Responsive Jumping Bug Fix

--- Assets/3D Character Test/3D Character/Scripts/ResponsiveJump.cs
+++ Assets/3D Character Test/3D Character/Scripts/ResponsiveJump.cs
@@ -4,6 +4,7 @@ public class ResponsiveJump : MonoBehaviour
 {
      [SerializeField] private float fallMultiplier;
      [SerializeField] private float lowJumpMultiplier;
+     [SerializeField] private float highJumpMultiplier;
 
      private Rigidbody _rb;
 
@@ -18,5 +19,7 @@ public class ResponsiveJump : MonoBehaviour
                _rb.velocity += Vector3.up * (Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime);
           else if (_rb.velocity.y > 0f && !InputsController.jumping)
                _rb.velocity += Vector3.up * (Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime);
+          else if (_rb.velocity.y > 0f && InputsController.jumping)
+               _rb.velocity += Vector3.up * (Physics.gravity.y * (highJumpMultiplier - 1) * Time.deltaTime);
      }
 }
